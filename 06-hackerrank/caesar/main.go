fdb80d667aa6d8dbb05b959815d4615fa0d7e660
Reiner Gerecke <me@reinergerecke.de>
2021-07-28 18:11:08 +0200

    Add hackerrank exercise

---
+++ 06-hackerrank/caesar/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+func caesarCipher(s string, k int32) string {
+	result := make([]rune, len(s))
+
+	for i, c := range s {
+		switch {
+		case ('a' <= c) && (c <= 'z'):
+			result[i] = (c-'a'+k)%26 + 'a'
+		case ('A' <= c) && (c <= 'Z'):
+			result[i] = (c-'A'+k)%26 + 'A'
+		default:
+			result[i] = c
+		}
+	}
+
+	return string(result)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	_, err = strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	s := readLine(reader)
+
+	kTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	result := caesarCipher(s, k)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ 06-hackerrank/go.mod
@@ -0,0 +1,3 @@
+module reinergerecke.de/gophercises/06-hackerrank
+
+go 1.16
